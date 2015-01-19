dd928dc295a90b2e5297ddc1b4d22d0aa449b372
Darren Boss <darren.boss@gmail.com>
2015-01-19 10:06:51 -0500

    Fixed code for setting the RTC

--- src/chicken_door_relay_board/chicken_door_relay_board.ino
+++ src/chicken_door_relay_board/chicken_door_relay_board.ino
@@ -61,11 +61,12 @@ void setup() {
   RTC.begin();
   if (! RTC.isrunning()) {
     Serial.println("RTC is NOT running!");
-    // following line sets the RTC to the date & time this sketch was compiled
-    RTC.adjust(DateTime(F(__DATE__), F(__TIME__)));
-    // This line sets the RTC with an explicit date & time, for example to set
-    // January 21, 2014 at 3am you would call:
-    // RTC.adjust(DateTime(2014, 1, 21, 3, 0, 0));
+  }
+  // following line sets the RTC to the date & time this sketch was compiled
+  RTC.adjust(DateTime(F(__DATE__), F(__TIME__)));
+  // This line sets the RTC with an explicit date & time, for example to set
+  // January 21, 2014 at 3am you would call:
+  // RTC.adjust(DateTime(2014, 1, 21, 3, 0, 0));
   }
   setSyncProvider(syncProvider);
   RTC.now;
